c6f07557bf5a9bed2877e7444ff15019f9aefee6
GustavoGomesDias <treeelevengg@gmail.com>
2020-12-26 01:54:34 -0300

    add user model and connect to db

---
+++ models/User.js
@@ -0,0 +1,22 @@
+// Modelo de User para o mongoDB
+// Como o model User vai parecer para o banco de dados
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
