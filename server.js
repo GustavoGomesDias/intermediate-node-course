7345708d480d98db299646715dfefa2b79cea241
GustavoGomesDias <treeelevengg@gmail.com>
2020-12-26 18:30:30 -0300

    add routes

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ const app = express();
 const User = require('./models/User');
 
 // Nos conecta a um DB mongoDB chamado userData
-mongoose.connect('mongodb://localhost/userData');
-
+// useNewUrlParser, useUnifiedTopology, useCreateIndex é só pra acertar dependecias
+mongoose.connect(
+  'mongodb://localhost/userData1', 
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+);
 app.use(bodyParser.json());
 
 app.listen(port, () =>{
@@ -17,13 +20,45 @@ app.listen(port, () =>{
 
 // CREATE
 app.post('/users',(req,res) =>{
-  // User.create()
+  User.create(
+    {
+      name: req.body.newData.name,
+      email: req.body.newData.email,
+      password: req.body.newData.password
+    },
+    (err, data) => {
+      if(err){
+        res.json({success: false, message: err});
+      }else if(!data){
+       res.json({success: false, message: "Not found"});
+      }else{
+        res.json({success: true, data: data});
+      }
+    }
+  );
 })
 
 app.route('/users/:id')
 // READ
 .get((req,res) =>{
-  // User.findById()
+  User.findById(req.params.id, (err, data) => {
+    if(err){
+      res.json({
+        success: false,
+        message: err
+      });
+    }else if(!data){
+      res.json({
+        success: false,
+        message: "Not Found"
+      });
+    }else{
+      res.json({
+        success: true,
+        data: data
+      });
+    }
+  });
 })
 // UPDATE
 .put((req,res) =>{
